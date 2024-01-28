d3362fe312229801624a29ca2b8684806ec10265
ForsakenSif <135348475+ForsakenSif@users.noreply.github.com>
2024-01-28 18:16:01 +0300

    Add files via upload

---
+++ src/test/java/io/proj3ct/SpringEduBot/SpringEduBotApplicationTests.java
@@ -0,0 +1,13 @@
+package io.proj3ct.SpringEduBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEduBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
