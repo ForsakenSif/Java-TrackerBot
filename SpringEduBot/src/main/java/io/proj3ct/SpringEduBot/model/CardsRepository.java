8dcfcc6f857d7821d10a8d019e0cd38d5228cf36
ForsakenSif <135348475+ForsakenSif@users.noreply.github.com>
2024-01-28 18:12:38 +0300

    Delete SpringEduBot directory

--- SpringEduBot/src/main/java/io/proj3ct/SpringEduBot/model/CardsRepository.java
+++
@@ -1,6 +0,0 @@
-package io.proj3ct.SpringEduBot.model;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface CardsRepository extends CrudRepository<Cards, Long> {
-}
--- SpringEduBot/src/main/java/io/proj3ct/SpringEduBot/model/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package io.proj3ct.SpringEduBot.model;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-}
